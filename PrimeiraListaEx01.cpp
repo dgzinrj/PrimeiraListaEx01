ab39077ff6c445a1dd4792aa11a52faea89163f6
Douglas dos Santos <douglasramos288@gmail.com>
2023-03-16 12:08:34 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiraListaEx01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+//Dpuglas dos Santos
+//Data: 16/03/2023
+//*O programa a seguir coloca na tela a frase: Vou aprender a programar na linguagem c++. Contudo
+//cada palavra será exibida em uma unica linha através da inserção de quebra de linhas*/
+
+int main()
+{
+	//cor de fundo 1
+	//cor da letra 9
+	system("COLOR 19");
+	std::cout << "Vou" << "\n"
+	<< "aprender" << "\n"
+	<< "a" << "\n"
+	<< "programar" << "\n"
+	<< "na" << "\n"
+	<< "linguagem c++" << "\n";
+	system("PAUSE");
+	return 0;
+}
